b69ae989d17c625820dcd8b4979878d93a004d93
Tuure Takala <tuure.takala@seamk.fi>
2020-05-09 10:25:05 +0300

    commit 2

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ const machining_controller = require('./machining_controller');
 app.use(body_parser.json()); //req.body.name
 app.use(body_parser.urlencoded({
     extended:true
-})); // material/id
+})); //
 
 app.use( (req, res, next)=>{
     console.log(req.method, ' ', req.path);
     next();
-}); //GET /api/materials
+});
 
 
 // RESTful API
@@ -29,15 +29,14 @@ app.get("/machining-parameter-sets", machining_controller.api_get_machining_para
 // UPDATE
 // app.patch korvaa vain tietyt kentät
 // app.put korvaa koko tiedon
-// app.put("/machining-parameter-set/:id", machining_controller.api_put_material);
+app.put("/machining-parameter-set/:id", machining_controller.api_put_machining_parameter_set);
 
 // DELETE
-// app.delete("/machining-parameter-set/:id", machining_controller.api_delete_material);
+app.delete("/machining-parameter-set/:id", machining_controller.api_delete_material);
 
 
-const database_uri ="mongodb+srv://machining-user:K518bR7grgQpZhFQ@cluster0-wfba5.mongodb.net/materialdb?retryWrites=true&w=majority"
-// "mongodb+srv://server:f1IFdTNtzuvzqZQS@cluster0-wfba5.mongodb.net/materialdb?retryWrites=true&w=majority"
-// 
+const database_uri ="mongodb+srv://machining-user:K518bR7grgQpZhFQ@cluster0-hubzr.mongodb.net/test?retryWrites=true&w=majority"
+ 
 // Mongo db connection
 mongoose.connect(database_uri, {
     useCreateIndex: true, 
--- machining_controller.js
+++ machining_controller.js
@@ -13,7 +13,7 @@ const machining_data = (req) => {
 
 // CREATE
 const api_post_machining_parameter_set = (req, res, next) => {
-
+    console.log('api_post_machining_parameter_set');
     let data = machining_data(req);
 
     let new_machining = machining_model(data);
@@ -47,41 +47,41 @@ const api_get_machining_parameter_sets = (req, res, next)=>{
 
 };
 
-// // UPDATE
-// // PUT /api/material/5e877019ab6c9e3a4868f634
-// const api_put_machining_parameter_set = (req, res, next) => {
-//     console.log('api_put_machining_parameter_set');
-//     let id = req.params.id;
-//     let data = machining_data(req);
+// UPDATE
+// PUT /api/material/5e877019ab6c9e3a4868f634
+const api_put_machining_parameter_set = (req, res, next) => {
+    console.log('api_put_machining_parameter_set');
+    let id = req.params.id;
+    let data = machining_data(req);
 
-//     material_model.findByIdAndUpdate(id, data, {
-//         new:true
-//     }).then((material) => {
-//         res.send(material);
-//     }).catch(err => {
-//         res.status(500);
-//         res.send(err.errmsg);
-//         console.log(err);
-//     });
-// };
+    machining_model.findByIdAndUpdate(id, data, {
+        new:true
+    }).then((material) => {
+        res.send(material);
+    }).catch(err => {
+        res.status(500);
+        res.send(err.errmsg);
+        console.log(err);
+    });
+};
 
-// // DELETE 
-// // DELETE /api/material/5e877019ab6c9e3a4868f634
-// const api_delete_machining_parameter_set = (req, res, next) => {
-//     let id = req.params.id;
-//     material_model.findByIdAndRemove(id).then(() => {
-//         res.send();
-//     }).catch(err => {
-//         res.status(500);
-//         res.send(err.errmsg);
-//         console.log(err);
-//     });
-// };
+// DELETE 
+// DELETE /api/material/5e877019ab6c9e3a4868f634
+const api_delete_machining_parameter_set = (req, res, next) => {
+    let id = req.params.id;
+    material_model.findByIdAndRemove(id).then(() => {
+        res.send();
+    }).catch(err => {
+        res.status(500);
+        res.send(err.errmsg);
+        console.log(err);
+    });
+};
 
 
 // EXPORTS
 module.exports.api_post_machining_parameter_set = api_post_machining_parameter_set; // lisää parametrisetti
 module.exports.api_get_machining_parameter_sets = api_get_machining_parameter_sets; // kysy kaikki parametrisetit
 // kysy tietty parametrisetti PUUTTUUU
-// module.exports.api_put_machining_parameter_set = api_put_machining_parameter_set; // päivitä parametrisetti
-// module.exports.api_delete_machining_parameter_set = api_delete_machining_parameter_set; // poista parametrisetti
+module.exports.api_put_machining_parameter_set = api_put_machining_parameter_set; // päivitä parametrisetti
+module.exports.api_delete_machining_parameter_set = api_delete_machining_parameter_set; // poista parametrisetti
